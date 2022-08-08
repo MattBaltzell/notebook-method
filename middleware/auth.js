94148a1cc84e6be894a880789e0bb6b9f24aa35e
Matt Baltzell <matthew.j.baltzell@gmail.com>
2022-08-08 13:12:09 -0500

    update middleware to include new custom errors and res.locals

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,27 +2,40 @@
 
 const jwt = require('jsonwebtoken');
 const { SECRET_KEY } = require('../config');
+const { UnauthorizedError } = require('../expressError');
 
-/** Middleware: Authenticate user. */
+/** Middleware: Authenticate user.
+ *
+ * If a token was provided, verify it, and, if valid, store the token payload
+ * on res.locals (this will include the username and isAdmin field.)
+ *
+ * It's not an error if no token was provided or if the token is not valid.
+ */
 
 function authenticateJWT(req, res, next) {
   try {
-    const tokenFromBody = req.body._token;
-    const payload = jwt.verify(tokenFromBody, SECRET_KEY);
-    req.user = payload; // create a current user
+    const authHeader = req.headers && req.headers.authorization;
+    if (authHeader) {
+      const token = authHeader.replace(/^[Bb]earer /, '').trim();
+      res.locals.user = jwt.verify(token, SECRET_KEY);
+    }
     return next();
   } catch (err) {
     return next();
   }
 }
 
-/** Middleware: Requires user is authenticated. */
+/** Middleware to use when they must be logged in.
+ *
+ * If not, raises Unauthorized.
+ */
 
 function ensureLoggedIn(req, res, next) {
-  if (!req.user) {
-    return next({ status: 401, message: 'Unauthorized' });
-  } else {
+  try {
+    if (!res.locals.user) throw new UnauthorizedError();
     return next();
+  } catch (err) {
+    return next(err);
   }
 }
 
@@ -30,31 +43,24 @@ function ensureLoggedIn(req, res, next) {
 
 function ensureCorrectUser(req, res, next) {
   try {
-    if (req.user.username === req.params.username) {
-      return next();
-    } else {
-      return next({ status: 401, message: 'Unauthorized' });
-    }
+    if (res.locals.user.username !== req.params.username)
+      throw new UnauthorizedError();
+    return next();
   } catch (err) {
-    // errors would happen here if we made a request and req.user is undefined
-    return next({ status: 401, message: 'Unauthorized' });
+    return next(err);
   }
 }
 
-/** Middleware: Requires user is authenticated. */
+/** Middleware: Requires user's user_type_id is 2 for teacher. */
 
 function ensureTeacher(req, res, next) {
   try {
-    if (req.user.user_type === 'teacher') {
-      return next();
-    } else {
-      return next({ status: 401, message: 'Unauthorized' });
-    }
+    const user = User.get(res.locals.user.username);
+    if (user.user_type_id !== 2) throw new UnauthorizedError();
   } catch (err) {
-    return next({ status: 401, message: 'Unauthorized' });
+    return next(err);
   }
 }
-// end
 
 module.exports = {
   authenticateJWT,
