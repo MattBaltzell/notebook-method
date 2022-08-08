9b41d3dedde84d4b7d78e800e7a3a3f53956f86a
Matt Baltzell <matthew.j.baltzell@gmail.com>
2022-08-08 13:22:59 -0500

    use NotFoundError, update 404 and general error handlers, reorder/organize page

--- app.js
+++ app.js
@@ -1,45 +1,43 @@
+'use strict';
+
 /** Express app. */
 
 const express = require('express');
 const cors = require('cors');
+
+const { NotFoundError } = require('./expressError');
+
 const { authenticateJWT } = require('./middleware/auth');
-
-const app = express();
-app.use(express.json());
-
-// allow connections to all routes from any browser
-app.use(cors());
-
-// get auth token for all routes
-app.use(authenticateJWT);
-
-/** routes */
 const authRoutes = require('./routes/auth');
 const teacherRoutes = require('./routes/teachers');
 const studentRoutes = require('./routes/students');
 const userRoutes = require('./routes/users');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(authenticateJWT);
 
 app.use('/auth', authRoutes);
 app.use('/teachers', teacherRoutes);
 app.use('/students', studentRoutes);
 app.use('/users', userRoutes);
 
-/** 404 handler */
-
+/** Handle 404 errors */
 app.use(function (req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-
-  // pass the error to the next piece of middleware
-  return next(err);
+  return next(new NotFoundError());
 });
 
-/** general error handler */
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+  if (process.env.NODE_ENV !== 'test') console.error(err.stack);
+  const status = err.status || 500;
+  const message = err.message;
 
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-
-  return res.send({ err });
+  return res.status(status).json({
+    error: { message, status },
+  });
 });
 
 module.exports = app;
