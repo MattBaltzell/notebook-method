714dccb91e637418a492badb57c8496c86c9bc0c
Matt Baltzell <matthew.j.baltzell@gmail.com>
2022-08-08 12:54:50 -0500

    added createToken helper function revamped auth.js route to use createToken()

---
+++ helpers/tokens.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const { SECRET_KEY } = require('../config');
+
+/** return signed JWT from user data. */
+
+function createToken(user) {
+  console.assert(
+    user.isTeacher !== undefined,
+    'createToken passed user without isTeacher property'
+  );
+
+  let payload = {
+    username: user.username,
+    isTeacher: user.isTeacher || false,
+  };
+
+  return jwt.sign(payload, SECRET_KEY);
+}
+
+module.exports = { createToken };
--- routes/auth.js
+++ routes/auth.js
@@ -1,46 +1,67 @@
+'use strict';
+
+/** Routes for authentication. */
+
+const jsonschema = require('jsonschema');
+
+const User = require('../models/user');
 const express = require('express');
 const router = new express.Router();
-const jwt = require('jsonwebtoken');
-const { SECRET_KEY } = require('../config.js');
-const ExpressError = require('../expressError.js');
-const User = require('../models/user.js');
+const { createToken } = require('../helpers/tokens');
+const userAuthSchema = require('../schemas/userAuth.json');
+const userRegisterSchema = require('../schemas/userRegister.json');
+const { BadRequestError } = require('../expressError');
 
-/** POST /login - login: {username, password} => {token}
+/** POST /auth/token:  { username, password } => { token }
  *
- * Make sure to update their last-login!
+ * Returns JWT token which can be used to authenticate further requests.
  *
- **/
+ * Authorization required: none
+ */
 
-router.post('/login', async (req, res, next) => {
+router.post('/token', async function (req, res, next) {
   try {
-    const { username, password } = req.body;
-    if (await User.authenticate(username, password)) {
-      let token = jwt.sign({ username }, SECRET_KEY);
-      User.updateLoginTimestamp(username);
-      return res.json({ token });
-    } else {
-      throw new ExpressError('Invalid username/password', 400);
+    const validator = jsonschema.validate(req.body, userAuthSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
     }
-  } catch (e) {
-    next(e);
+
+    const { username, password } = req.body;
+    const user = await User.authenticate(username, password);
+    User.updateLoginTimestamp(username);
+
+    const token = createToken(user);
+    return res.json({ token });
+  } catch (err) {
+    return next(err);
   }
 });
 
-/** POST /register - register User: registers, logs in, and returns token.
+/** POST /auth/register:   { user } => { token }
  *
- * {username, email, password, first_name, last_name} => {token}.
+ * user must include { username, password, first_name, last_name, email }
  *
- *  Make sure to update their last-login!
+ * Returns JWT token which can be used to authenticate further requests.
+ *
+ * Authorization required: none
  */
 
-router.post('/register', async (req, res, next) => {
+router.post('/register', async function (req, res, next) {
   try {
-    const { username } = await User.register(req.body);
-    let token = jwt.sign({ username }, SECRET_KEY);
-    User.updateLoginTimestamp(username);
-    return res.send({ token });
-  } catch (e) {
-    next(e);
+    const validator = jsonschema.validate(req.body, userRegisterSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const newUser = await User.register({ ...req.body, isAdmin: false });
+    User.updateLoginTimestamp(req.body.username);
+
+    const token = createToken(newUser);
+    return res.status(201).json({ token });
+  } catch (err) {
+    return next(err);
   }
 });
 
