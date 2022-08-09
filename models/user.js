e1548b524ad7bd0455b890110406ea636271e583
Matt Baltzell <matthew.j.baltzell@gmail.com>
2022-08-09 15:05:40 -0500

    import various erros from ExpressError Add duplicateCheck to register method Update authenticate method to select more data in query Create isValid variable to check if username/password is valid Add error handling to User methods.

--- models/user.js
+++ models/user.js
@@ -1,7 +1,11 @@
 /** User  */
 
 const db = require('../db');
-const ExpressError = require('../expressError');
+const {
+  NotFoundError,
+  UnauthorizedError,
+  BadRequestError,
+} = require('../expressError');
 const bcrypt = require('bcrypt');
 const { BCRYPT_WORK_FACTOR } = require('../config.js');
 
@@ -9,59 +13,93 @@ class User {
   /** register new user */
 
   static async register({ username, email, password, firstName, lastName }) {
+    const duplicateCheck = await db.query(
+      `SELECT username
+         FROM users
+         WHERE username = $1`,
+      [username]
+    );
+
+    if (duplicateCheck.rows[0]) {
+      throw new BadRequestError(`Duplicate username: ${username}`);
+    }
+
     const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
-    const results = await db.query(
+
+    const result = await db.query(
       `INSERT INTO users (
-        username, 
-        user_type_id, 
-        email, 
-        password,
-        first_name,
-        last_name,
-        join_at ) 
-        VALUES ($1, 1, $2, $3, $4, $5, CURRENT_TIMESTAMP) 
-        RETURNING id, username, user_type_id AS "userTypeID", email, first_name AS "firstName", last_name AS "lastName", join_at AS joinAt`,
+      username, 
+      user_type_id, 
+      email, 
+      password,
+      first_name,
+      last_name,
+      join_at ) 
+      VALUES ($1, 1, $2, $3, $4, $5, CURRENT_TIMESTAMP) 
+      RETURNING username, email, first_name AS "firstName", last_name AS "lastName"`,
       [username, email, hashedPassword, firstName, lastName]
     );
 
-    return results.rows[0];
+    const user = result.rows[0];
+
+    return user;
   }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
-    const results = await db.query(
-      'SELECT password FROM users WHERE username=$1',
+    const result = await db.query(
+      `SELECT username,
+              password,
+              first_name AS "firstName",
+              last_name AS "lastName",
+              email,
+              user_type_id AS "userTypeID"
+           FROM users
+           WHERE username = $1`,
       [username]
     );
-    const user = results.rows[0];
+    const user = result.rows[0];
 
     if (user) {
-      return await bcrypt.compare(password, user.password);
+      // compare hashed password to a new hash from password
+      const isValid = await bcrypt.compare(password, user.password);
+      if (isValid === true) {
+        delete user.password;
+        return user;
+      }
     }
+
+    throw new UnauthorizedError('Invalid username/password');
   }
 
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
-    const results = await db.query(
+    const result = await db.query(
       'UPDATE users SET last_login_at=NOW() WHERE username=$1',
       [username]
     );
+    const user = result.rows[0];
+    if (!user) throw new NotFoundError(`No user with userID of ${userID}`);
   }
 
   static async updateUserType(userTypeID, userID) {
-    await db.query(
+    const result = await db.query(
       `UPDATE users
         SET user_type_id=$1
-        WHERE id=$2`,
+        WHERE id=$2
+        RETURNING id, user_type_id AS "userTypeID"`,
       [userTypeID, userID]
     );
+
+    const user = result.rows[0];
+    if (!user) throw new NotFoundError(`No user with userID of ${userID}`);
   }
 
   /** get all users */
   static async getAll() {
-    const results = await db.query(
+    const result = await db.query(
       `SELECT 
           id,
           username,
@@ -76,12 +114,12 @@ class User {
         ORDER BY id`
     );
 
-    return results.rows;
+    return result.rows;
   }
 
   /** get user by username */
   static async get(username) {
-    const results = await db.query(
+    const result = await db.query(
       `SELECT 
         id,
         username,
@@ -97,15 +135,15 @@ class User {
       [username]
     );
 
-    if (!results.rows[0]) {
-      throw new ExpressError(`User not found`, 404);
-    }
-    return results.rows[0];
+    const user = result.rows[0];
+    if (!user) throw new NotFoundError(`No user: ${username}`);
+
+    return user;
   }
 
   /** update a user */
   static async update({ username, email, firstName, lastName, avatarURL }) {
-    const results = await db.query(
+    const result = await db.query(
       `UPDATE users 
         SET
           username = $1, 
@@ -118,19 +156,26 @@ class User {
       [username, email, firstName, lastName, avatarURL]
     );
 
-    return results.rows[0];
+    const user = result.rows[0];
+    if (!user) throw new NotFoundError(`No user: ${username}`);
+
+    return user;
   }
 
   /** delete a user */
   static async delete(username) {
-    const results = await db.query(
+    const result = await db.query(
       `DELETE
         FROM users 
         WHERE username ILIKE $1
         RETURNING username`,
       [username]
     );
-    return results.rows[0];
+
+    const user = result.rows[0];
+    if (!user) throw new NotFoundError(`No user: ${username}`);
+
+    return user;
   }
 }
 
