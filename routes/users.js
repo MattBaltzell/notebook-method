69c30b76589b54358683f10f4c0b0f50b241d2dc
Matt Baltzell <matthew.j.baltzell@gmail.com>
2022-08-12 10:58:45 -0500

    add POST /users to allow admins to create users

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,50 @@
 const jsonschema = require('jsonschema');
 
 const express = require('express');
-const { ensureLoggedIn, ensureCorrectUser } = require('../middleware/auth');
+const {
+  ensureLoggedIn,
+  ensureAdmin,
+  ensureCorrectUser,
+} = require('../middleware/auth');
 const {
   NotFoundError,
   UnauthorizedError,
   BadRequestError,
 } = require('../expressError');
-4;
+const { createToken } = require('../helpers/tokens');
 const userUpdateSchema = require('../schemas/userUpdate.json');
+const userNewSchema = require('../schemas/userNew.json');
 const User = require('../models/user');
 
 const router = new express.Router();
+
+/** POST / { user }  => { user, token }
+ *
+ * Adds a new user. This is not the registration endpoint --- instead, this is
+ * only for admin users to add new users. The new user being added can be an
+ * admin.
+ *
+ * This returns the newly created user and an authentication token for them:
+ *  {user: { username, firstName, lastName, email, isAdmin }, token }
+ *
+ * Authorization required: admin
+ **/
+
+router.post('/', ensureAdmin, async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, userNewSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const user = await User.register(req.body);
+    const token = createToken(user);
+    return res.status(201).json({ user, token });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 /** Show all users */
 
