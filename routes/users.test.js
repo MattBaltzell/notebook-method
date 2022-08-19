a48a8325eb59ff3d6188d8fdd3f416ee01114a57
Matt Baltzell <matthew.j.baltzell@gmail.com>
2022-08-19 16:29:37 -0500

    add tests to multiple routes

--- routes/users.test.js
+++ routes/users.test.js
@@ -21,15 +21,15 @@ afterAll(commonAfterAll);
 /************************************** POST /users */
 
 describe('POST /users', function () {
+  const newUser = {
+    username: 'newAdminUser',
+    email: 'newadminuser@email.com',
+    password: 'password',
+    firstName: 'New',
+    lastName: 'Admin',
+    isAdmin: true,
+  };
   test('works: admin can create new admin', async function () {
-    const newUser = {
-      username: 'newAdminUser',
-      email: 'newadminuser@email.com',
-      password: 'password',
-      firstName: 'New',
-      lastName: 'Admin',
-      isAdmin: true,
-    };
     const resp = await request(app)
       .post('/users')
       .send(newUser)
@@ -47,13 +47,48 @@ describe('POST /users', function () {
       token: expect.any(String),
     });
   });
+
+  test('bad request when missing parameters', async function () {
+    const resp = await request(app)
+      .post('/users')
+      .send({
+        username: 'newAdminUser',
+        email: 'newadminuser@email.com',
+        password: 'password',
+      })
+      .set('authorization', `Bearer ${a1Token}`);
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  test('bad request when attempting to update invalid parameter', async function () {
+    const resp = await request(app)
+      .post('/users')
+      .send({ ...newUser, eatsMeat: false })
+      .set('authorization', `Bearer ${a1Token}`);
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  test('unauth for non-admins', async function () {
+    const resp = await request(app)
+      .post('/users')
+      .send(newUser)
+      .set('authorization', `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(401);
+  });
+
+  test('unauth for anon', async function () {
+    const resp = await request(app).post('/users').send(newUser);
+    expect(resp.statusCode).toEqual(401);
+  });
 });
 
 /************************************** GET /users */
 
 describe('GET /users', function () {
   test('works', async function () {
-    const resp = await request(app).get('/users');
+    const resp = await request(app)
+      .get('/users')
+      .set('authorization', `Bearer ${a1Token}`);
     expect(resp.body).toEqual({
       users: [
         {
@@ -143,6 +178,18 @@ describe('GET /users', function () {
       ],
     });
   });
+
+  test('unauth for non-admins', async function () {
+    const resp = await request(app)
+      .get('/users')
+      .set('authorization', `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(401);
+  });
+
+  test('unauth for anon', async function () {
+    const resp = await request(app).get('/users');
+    expect(resp.statusCode).toEqual(401);
+  });
 });
 
 /************************************** GET /users/:username */
@@ -166,6 +213,45 @@ describe('GET /users/:username', function () {
         isAdmin: false,
       },
     });
+  });
+
+  test('works: as admin', async function () {
+    const resp = await request(app)
+      .get('/users/u1')
+      .set('authorization', `Bearer ${a1Token}`);
+    expect(resp.body).toEqual({
+      user: {
+        id: expect.any(Number),
+        username: 'u1',
+        email: 'user1@user.com',
+        firstName: 'U1F',
+        lastName: 'U1L',
+        userTypeID: 1,
+        avatarURL: null,
+        joinAt: expect.any(String),
+        lastLoginAt: null,
+        isAdmin: false,
+      },
+    });
+  });
+
+  test('unauth if wrong user', async function () {
+    const resp = await request(app)
+      .get('/users/u2')
+      .set('authorization', `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(401);
+  });
+
+  test('unauth for anon', async function () {
+    const resp = await request(app).get('/users/u2');
+    expect(resp.statusCode).toEqual(401);
+  });
+
+  test('as admin: not found when username does not exist', async function () {
+    const resp = await request(app)
+      .get('/users/imposter')
+      .set('authorization', `Bearer ${a1Token}`);
+    expect(resp.statusCode).toEqual(404);
   });
 });
 
@@ -211,12 +297,52 @@ describe('PATCH /users/:username', function () {
     });
   });
 
-  test('auth error when attempting to edit another user', async function () {
+  test('works: as admin', async function () {
+    const resp = await request(app)
+      .patch('/users/u1')
+      .send({ avatarURL: 'https://thisisatestlink.com/img' })
+      .set('authorization', `Bearer ${a1Token}`);
+    expect(resp.body).toEqual({
+      user: {
+        username: 'u1',
+        email: 'user1@user.com',
+        firstName: 'U1F',
+        lastName: 'U1L',
+        avatarURL: 'https://thisisatestlink.com/img',
+        isAdmin: false,
+      },
+    });
+  });
+
+  test('bad request when attempting to update invalid parameter', async function () {
+    const resp = await request(app)
+      .patch('/users/u2')
+      .send({ lovesChickens: true })
+      .set('authorization', `Bearer ${a1Token}`);
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  test('unauth when attempting to edit another user', async function () {
     const resp = await request(app)
       .patch('/users/u2')
       .send({ avatarURL: 'https://thisisatestlink.com/img' })
       .set('authorization', `Bearer ${u1Token}`);
     expect(resp.statusCode).toEqual(401);
+  });
+
+  test('unauth for anon', async function () {
+    const resp = await request(app)
+      .patch('/users/u2')
+      .send({ avatarURL: 'https://thisisatestlink.com/img' });
+    expect(resp.statusCode).toEqual(401);
+  });
+
+  test('as admin: not found when username does not exist', async function () {
+    const resp = await request(app)
+      .patch('/users/imposter')
+      .send({ avatarURL: 'https://thisisatestlink.com/img' })
+      .set('authorization', `Bearer ${a1Token}`);
+    expect(resp.statusCode).toEqual(404);
   });
 });
 
@@ -228,5 +354,31 @@ describe('DELETE /users/:username', function () {
       .delete('/users/u1')
       .set('authorization', `Bearer ${u1Token}`);
     expect(resp.body).toEqual({ message: 'Deleted.' });
+  });
+
+  test('works: as admin', async function () {
+    const resp = await request(app)
+      .delete('/users/u1')
+      .set('authorization', `Bearer ${a1Token}`);
+    expect(resp.body).toEqual({ message: 'Deleted.' });
+  });
+
+  test('unauth when attempting to delete another user', async function () {
+    const resp = await request(app)
+      .delete('/users/u2')
+      .set('authorization', `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(401);
+  });
+
+  test('unauth for anon', async function () {
+    const resp = await request(app).delete('/users/u2');
+    expect(resp.statusCode).toEqual(401);
+  });
+
+  test('admin: not found when username does not exist', async function () {
+    const resp = await request(app)
+      .delete('/users/imposter')
+      .set('authorization', `Bearer ${a1Token}`);
+    expect(resp.statusCode).toEqual(404);
   });
 });
